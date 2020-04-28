fc86728c6be3b91df361746ff2c827d3c3cc5091
gilberto t <gilbertorosemont177@gmail.com>
2020-04-28 11:43:30 -0700

    mysql connection works with nodejs

--- app-nodeapi/index.js
+++ app-nodeapi/index.js
@@ -1,28 +1,28 @@
 const express = require('express')
 const app = express()
+var mysql = require('mysql')
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var dbconnection = mysql.createConnection({
+  host: '0.0.0.0',
+  user: 'root',
+  password: 'root',
+  database: 'USERSTEST',
+  port:7006
+});
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+dbconnection.connect();
+  
+app.listen(port, () => {console.log("liste port 3000")})
 
+app.get('/',(req, res) => {
 
-/*
-var mysql = require('mysql')
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'dbuser',
-  password: 's3kreee7',
-  database: 'my_db'
-})
-
-connection.connect()
-
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-  if (err) throw err
-
-  console.log('The solution is: ', rows[0].solution)
-})
-
-connection.end()
-*/
+  let query="SELECT * FROM LoginUsers";
+  dbconnection.query(query, (err, rows)=> {
+    if (err) throw err;
+  //console.log('helloworld 0 ')
+ console.log('username => ', rows[0].usernames)
+  })
+  //connection.end()
+  res.send('Hello World! 1')
+});
